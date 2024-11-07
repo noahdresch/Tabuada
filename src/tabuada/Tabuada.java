51e409317cb68ec87f68e3a4523d7d3a64fd5dc0
NoahDresch <dreschnoah@gmail.com>
2024-11-07 09:34:11 -0300

    commit projeto

---
+++ src/tabuada/Tabuada.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tabuada;
+
+/**
+ *
+ * @author Noa
+ */
+public class Tabuada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 4;
+        int cont = 1;
+        
+        
+        while (cont <= 10) {
+            System.out.println(cont + "*4" + "=" + cont * 4);
+        
+        
+        cont++;
+    }
+        
+    }
+        
+    
+}
